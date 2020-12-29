5c08961133d6f1517e307d3b2e9468875f120128
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-28 20:02:03 -0400

    Ensured that we stop tracking location if user is tabbed out from TrackCreate screen

--- path-client/src/screens/TrackCreate.screen.jsx
+++ path-client/src/screens/TrackCreate.screen.jsx
@@ -1,51 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import { SafeAreaView } from "react-navigation";
+import { SafeAreaView, withNavigationFocus } from "react-navigation";
 import { Map } from "../components";
-import {
-  requestPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from "expo-location";
 import "../_mockLocation";
 import { Context as LocationContext } from "../contexts/location.context";
+import useLocation from "../hooks/useLocation";
 
-const TrackCreate = () => {
-  const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
+const TrackCreate = ({ isFocused }) => {
   const {} = styles;
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          addLocation(location);
-        }
-      );
-    } catch (error) {
-      setErr(error);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  const { addLocation } = useContext(LocationContext);
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h3>TrackCreate</Text>
       <Map />
+      {/* <NavigationEvents onWillBlur={() => console.log("im closing")} /> */}
       {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
@@ -53,4 +25,4 @@ const TrackCreate = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreate;
+export default withNavigationFocus(TrackCreate);
