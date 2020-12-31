a4105af6152eca548745250df4691ca5db615ae5
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-31 12:37:02 -0400

    Populated TrackList with tracks saved in backend, added navigation to TrackDetail screen

--- path-client/src/screens/TrackList.screen.jsx
+++ path-client/src/screens/TrackList.screen.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { Context as TrackContext } from "../contexts/track.context";
+import { ListItem } from "react-native-elements";
 
 const TrackList = ({ navigation: { navigate } }) => {
-  const {} = styles;
+  const { state, fetchTracks } = useContext(TrackContext);
 
   return (
     <>
-      <Text>TrackList</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => {
-          navigate("TrackDetail");
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                navigate("TrackDetail", { _id: item._id });
+              }}
+            >
+              <ListItem bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          );
         }}
       />
     </>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default TrackList;
