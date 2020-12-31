e863abbf4fa60361c433bb441b3fe6543f5025b9
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-31 11:48:05 -0400

    Modified our API call to prepend our Authorization Bearer to all calls.

--- path-client/src/api/tracker.js
+++ path-client/src/api/tracker.js
@@ -1,7 +1,21 @@
 import axios from "axios";
-import { proc } from "react-native-reanimated";
+import { AsyncStorage } from "@react-native-async-storage/async-storage";
 
-export default axios.create({
-  // baseURL: process.env.NGROK_URI,
+const instance = axios.create({
   baseURL: process.env.NGROK_URI,
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
