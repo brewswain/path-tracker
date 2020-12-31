38b0b26ecff8fdcd4da72bbe1533d1818157997f
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-31 12:46:23 -0400

    Show Track details when user navigates to selected Track in TrackList.

--- path-client/src/screens/TrackDetail.screen.jsx
+++ path-client/src/screens/TrackDetail.screen.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { Context as TrackContext } from "../contexts/track.context";
+import MapView, { Polyline } from "react-native-maps";
+const TrackDetail = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const { map } = styles;
 
-const TrackDetail = () => {
-  const {} = styles;
+  const _id = navigation.getParam("_id");
+
+  const track = state.find((item) => item._id === _id);
+  const initialCoords = track.locations[0].coords;
 
   return (
-    <View>
-      <Text>TrackDetail</Text>
-    </View>
+    <>
+      <Text>{track.name}</Text>
+      <MapView
+        style={map}
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords,
+        }}
+      >
+        <Polyline
+          coordinates={track.locations.map((location) => location.coords)}
+        />
+      </MapView>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default TrackDetail;
